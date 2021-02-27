76a9a02cccf457fd2c89a7eb5413f78ee599a4c4
Siraj <sirajchokshi@gmail.com>
2021-02-27 12:59:15 -0600

    add fix mongo URI

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+console.log(process.env.MONGO_URI);
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
@@ -14,4 +16,4 @@ app.use(express.json());
 const coursesRouter = require('./routes/courses');
 app.use('/courses', coursesRouter);
 
-app.listen(3000, () => console.log('server started'));
+app.listen(8000, () => console.log('server started'));
