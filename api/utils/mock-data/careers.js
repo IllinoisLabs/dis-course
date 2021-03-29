8aebb4fcd42fa8ca5328a800131eff1ddcb89673
jonathanshi568 <jonathan08082008@gmail.com>
2021-03-29 13:39:40 -0700

    Insert course mock data

---
+++ api/utils/mock-data/careers.js
@@ -0,0 +1,13 @@
+const Career = require('../../models/career');
+
+console.log(process.argv[2]);
+
+num = process.argv[2];
+
+
+// const career = new Career({
+//   id: hashId(req.body.name + req.body.desc),
+//   name: req.body.name,
+//   courses: req.body.courses,
+//   desc: req.body.desc,
+// });
